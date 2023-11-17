9156c295cd52f18f4fc3b75c5e9e511185589f6c
Stanislav <stanislav.pogorskii@gmail.com>
2023-11-17 12:30:14 +0400

    fix

--- app/video-games/games/page.tsx
+++ app/video-games/games/page.tsx
@@ -27,7 +27,7 @@ export default async function Page({
 
   return (
     <>
-      <div className="mb-4">
+      <div className="sticky top-0 mb-4">
         <Search placeholder="Search any game" />
       </div>
       <div key={uuid()} className="grid grid-cols-2 gap-6">
